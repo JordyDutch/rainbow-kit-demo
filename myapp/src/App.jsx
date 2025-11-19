7fb6b594815b43a4489304b7feaacf7a0964a9e5
jordydutch <jordy@sigmatic.io>
2025-11-19 15:33:21 +0100

    Push fix

---
+++ myapp/src/App.jsx
@@ -0,0 +1,14 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>RainbowKit Demo</h1>
+      <ConnectButton />
+    </div>
+  )
+}
+
+export default App
+
